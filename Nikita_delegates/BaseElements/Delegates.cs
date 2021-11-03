73309cf52e3182ac5125129cf19cf4d2367ba5db
NikitaKharashun <kharashun.nikita73@gmail.com>
2021-11-03 21:41:36 +0300

    update

---
+++ Nikita_delegates/BaseElements/Delegates.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nikita_delegates.BaseElements
+{
+    class Delegates
+    {
+        public delegate int Value();
+        public delegate int AverageValue(Value[] mass);
+
+        static readonly Random rnd = new Random();
+        public static int RandomNumber() => rnd.Next(1, 100);
+
+        public static Value[] Setter(int size)
+        {
+            Value[] mass = size > 0 ? new Value[size] : new Value[1];
+
+            for (int i = 0; i < mass.Length; i++)
+                mass[i] = RandomNumber;
+
+            return mass;
+        }
+
+        public static AverageValue averageValue = delegate (Value[] mass)
+        {
+            int sum = 0;
+            foreach (var item in mass)
+                sum += item.Invoke();
+
+            Console.WriteLine($"Сумма всех значений: {sum}");
+            return sum / mass.Length;
+        };
+    }
+}
--- Nikita_delegates/Program.cs
+++ Nikita_delegates/Program.cs
@@ -3,49 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nikita_delegates.BaseElements;
 
 namespace Nikita_delegates
 {
     class Program
     {
-        public delegate int Value();
-        public delegate int AverageValue(Value[] mass);
-
-
-        static readonly Random rnd = new Random();
-        public static int RandomNumber() => rnd.Next(1, 100);
-        public static Value[] Setter(int size)
-        {
-            Value[] mass = size > 0 ? new Value[size] : new Value[1];
-
-            for (int i = 0; i < mass.Length; i++)
-            {
-                mass[i] = RandomNumber;
-                Console.WriteLine($"delegate № {i + 1} = {mass[i].Invoke()}");
-            }
-            return mass;
-        }
-
-
         static void Main(string[] args)
         {
             Console.WriteLine("Введите размер массива");
             int.TryParse(Console.ReadLine(), out int size);
 
-            Setter(size);
-
             Console.WriteLine(new string('-', 40));
-            Console.WriteLine($"Сумма значений: {AverageValueMeth(Setter(size))}");
+            Console.WriteLine($"Среднее значение: {Delegates.averageValue(Delegates.Setter(size))}");
 
             Console.ReadKey();
-        }
-
-        public static int AverageValueMeth(Value[] mass)
-        {
-            int sum = 0;
-            for (int i = 0; i < mass.Length; i++)
-                sum += mass[i]();
-            return sum / mass.Length;
         }
     }
 }   
